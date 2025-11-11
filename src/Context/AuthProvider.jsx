f25a89fe8134226070d1b469926f796c4c63c347
wp-shuvo <shuvoalshaied@gmail.com>
2025-11-11 16:06:59 +0600

    [Added] Login & logout Function

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,8 +1,76 @@
-import React, { Children } from 'react';
+import React, { Children, useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { auth } from '../FireBase/firebase.init';
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ Children }) => {
-  const authinfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setloading] = useState(true);
+  const [errorInvalid, setErrorInvalid] = useState('');
+
+  // create user with email&pass
+
+  const createUser = (email, password) => {
+    setloading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Login USER
+  const singInuser = (email, password) => {
+    setloading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // login With Google Account
+  const singInWithGoogle = () => {
+    setloading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // SingOut or LogOut user
+  const singOutUser = () => {
+    setloading(true);
+    return signOut(auth);
+  };
+
+  //Get Current User Info
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      // console.log('in auth state change', currentUser);
+      setUser(currentUser);
+      setloading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // forget password
+  const forgetPassword = email => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  const authinfo = {
+    createUser,
+    user,
+    loading,
+    singInuser,
+    singInWithGoogle,
+    singOutUser,
+    forgetPassword,
+    errorInvalid,
+    setErrorInvalid,
+  };
   return <AuthContext value={authinfo}>{Children}</AuthContext>;
 };
 
